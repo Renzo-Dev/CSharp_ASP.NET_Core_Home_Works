c1facb103853715b728d5c0c1e6a7a18f6755c34
Renzo-Dev <dankoshevoy2001@gmail.com>
2023-07-13 08:09:05 +0300

    update

--- Home Work 2/Program.cs
+++ Home Work 2/Program.cs
@@ -1,8 +1,5 @@
 // создаем объект класс строитель ( WebApplications )
 
-
-using System.Text.Json.Serialization;
-
 var build = WebApplication.CreateBuilder(args);
 
 var app = build.Build();
@@ -12,36 +9,48 @@ app.UseStaticFiles();
 app.Run(async (ctx) =>
 {
     var path = Uri.UnescapeDataString(ctx.Request.Path);
-    string? query = ctx.Request.QueryString.ToString();
-    Console.WriteLine(query);
+    var query = ctx.Request.QueryString.ToString();
 
-    if (query == "?restaurants")
+    if (query == "?countries")
+    {
+        List<Country> countries = new List<Country>
+        {
+            new Country("Соединенные Штаты Америки (США)", "Вашингтон, округ Колумбия", "Северная Америка", 331_000_000, "английский"),
+            new Country("Франция", "Париж", "Европа", 67_000_000, "французский"),
+            new Country("Китай", "Пекин", "Азия", 1_400_000_000, "китайский"),
+            new Country("Бразилия", "Бразилиа", "Южная Америка", 213_000_000, "португальский"),
+            new Country("Япония", "Токио", "Азия", 126_000_000, "японский")
+        };
+        ctx.Response.Headers.ContentType = "application/json; charset=utf-8";
+        await ctx.Response.WriteAsJsonAsync(countries);
+    }
+    else if (query == "?restaurants")
     {
         List<Restaurant> restaurants = new List<Restaurant>(new[]
         {
             new Restaurant("Гурманское наслаждение",
                 "Изысканный ресторан, где вы сможете насладиться изысканными блюдами и неповторимой атмосферой.",
-                "Ukraine", "Kiev"),
+                "Украина", "Киев"),
             new Restaurant("Вкусные секреты",
-                "Уютное место, где каждое блюдо приготовлено с любовью и вниманием к деталям", "Ukraine", "Kiev"),
+                "Уютное место, где каждое блюдо приготовлено с любовью и вниманием к деталям", "Украина", "Киев"),
             new Restaurant("Рестофесть",
                 "Ресторан с прекрасным видом, где вы сможете насладиться великолепными блюдами и потрясающими закатами.",
-                "Ukraine", "Kiev"),
+                "Украина", "Киев"),
             new Restaurant("Гастрономический рай",
                 "Инновационный ресторан, где вас ждут смелые сочетания вкусов и оригинальные кулинарные решения.",
-                "Ukraine", "Kiev"),
+                "Украина", "Киев"),
             new Restaurant("Ароматные кулинарные чудеса",
                 "Место, где гастрономические вкусы встречаются с традиционными рецептами, чтобы создать неповторимый опыт.",
-                "Ukraine", "Kiev"),
+                "Украина", "Киев"),
             new Restaurant("Кулинарное путешествие",
                 "Ресторан, где каждое блюдо - это произведение искусства, сочетающее в себе элегантность и восхитительный вкус.",
-                "Ukraine", "Kiev"),
+                "Украина", "Киев"),
             new Restaurant("Сладкий уголок",
-                "Оазис для гурманов, предлагающий широкий выбор изысканных блюд и отличный сервис.", "Ukraine",
-                "Kiev"),
+                "Оазис для гурманов, предлагающий широкий выбор изысканных блюд и отличный сервис.", "Украина",
+                "Киев"),
             new Restaurant("Уютная трапеза",
                 "Ресторан, где деликатесы и свежие ингредиенты воплощаются в блюдах, вызывающих настоящий восторг.",
-                "Ukraine", "Kiev"),
+                "Украина", "Киев"),
         });
 
         ctx.Response.Headers.ContentType = "application/json; charset=utf-8";
@@ -93,3 +102,4 @@ app.Run(async (ctx) =>
 app.Run();
 
 public record Restaurant(string? Name,string Description,string Country,string City);
+public record Country(string Name, string Capital, string Continent, int Population, string OfficialLanguage);
