d9841cb89cfb50052e6ba19769fd07c5ec8979b8
Renzo-Dev <dankoshevoy2001@gmail.com>
2023-07-26 03:29:05 +0300

    Tasks: доделать загрузку, изменение, добавление, удаление.

--- Home Work 3/Program.cs
+++ Home Work 3/Program.cs
@@ -3,9 +3,10 @@ using Home_Work_3;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+// создаем пользователей ( временная БД ) 
 var users = new List<User>
 {
-    // new(){ Guid = Guid.NewGuid().ToString(),FirstName = "Dan",LastName = "Koshevoy",},
     new(Guid.NewGuid().ToString(), "Dan", "Koshevoy", 22, new DateTime(2001, 7, 19)),
     new(Guid.NewGuid().ToString(), "John", "Doe", 35, new DateTime(2023, 7, 23)),
     new(Guid.NewGuid().ToString(),"Alice", "Smith", 29, new DateTime(2023, 7, 23)),
@@ -15,18 +16,30 @@ var users = new List<User>
 
 app.UseStaticFiles();
 
+// 
+
+app.Map("/users", GetAllUsers);
 app.Map("/Task1", Task1);
 app.Map("/Task2", Task2);
 app.Run(async (ctx) =>
 {
-    var path = Uri.UnescapeDataString(ctx.Request.Path);
-    
+    Console.WriteLine(ctx.Request.Path);
     ctx.Response.ContentType = "text/html; charset=UTF-8";
     await ctx.Response.SendFileAsync("Pages/index.html");
 });
 
 app.Run();
 
+// 
+void GetAllUsers(IApplicationBuilder app)
+{
+    app.Run(async ctx =>
+    {
+        await ctx.Response.WriteAsJsonAsync(users);
+    });
+}
+
+// первая страница с заданием к ДЗ
 static void Task1(IApplicationBuilder app)
 {
     app.Run(async ctx =>
@@ -36,6 +49,7 @@ static void Task1(IApplicationBuilder app)
     });
 }
 
+// вторая страница с заданием к ДЗ
 static void Task2(IApplicationBuilder app)
 {
     app.Run(async ctx => { await ctx.Response.SendFileAsync("Pages/Task2.html"); });
