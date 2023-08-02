a91ac355d6d6f5e19cd4116e0e984fb817600c6e
Renzo-Dev <dankoshevoy2001@gmail.com>
2023-08-02 03:10:36 +0300

    1

---
+++ Home Work 4/Pages/Task1.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Home_Work_4.Pages;
+
+public class Task1 : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Home Work 4/Pages/index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Home_Work_4.Pages;
+
+public class index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
