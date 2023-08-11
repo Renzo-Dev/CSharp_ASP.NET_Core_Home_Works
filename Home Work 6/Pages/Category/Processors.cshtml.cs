82bbe15c2f1a42ba729271a6f1400f186957fe48
Renzo-Dev <dankoshevoy2001@gmail.com>
2023-08-11 13:33:15 +0300

    up

---
+++ Home Work 6/Pages/Category/Processors.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Home_Work_6.Pages.Category;
+
+public class Processors : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- Home Work 6/Pages/Index.cshtml.cs
+++ Home Work 6/Pages/Index.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Home_Work_6.Pages;
 
-public class index : PageModel
+public class Index : PageModel
 {
     public void OnGet()
     {
