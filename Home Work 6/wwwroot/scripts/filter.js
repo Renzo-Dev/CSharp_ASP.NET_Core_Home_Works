d43a5ebb37e752aa865eb59b4741d3c003d33510
Administrator <dankoshevoy2001@gmail.com>
2023-08-17 16:37:28 +0300

    update

--- Home Work 6/wwwroot/scripts/filter.js
+++ Home Work 6/wwwroot/scripts/filter.js
@@ -1,49 +1,79 @@
 window.addEventListener('load', () => {
-    // получаем список продуктов
-    const products = document.querySelectorAll(".show__processor__container");
-    // панелька фильтра
-    const filter_panel = document.querySelector(".filter__panel__container");
-    // получаем массив checkbox
-    const checkboxes = filter_panel.querySelectorAll("input");
-    const filter_names = filter_panel.querySelectorAll("span");
-    let filters = [];
 
-    checkboxes.forEach((chBox, index) => {
-        chBox.addEventListener('change', () => {
-            setFilter(chBox, index, filter_names, filters, products);
+    // получаем исходный список продуктов ( без последующих обращений к серверу, только если перезагрузим страницу )
+    const SourceProducts = document.querySelectorAll(".show__processor__container");
+
+    renderProducts();
+    document.querySelectorAll(".filter__panel__container input").forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            renderProducts();
         });
     });
-
-    // добавляем activeCheckBox в фильтр
-    function setFilter(chBox, _index, filter_names, filters, products) {
-        if (chBox.checked) {
-            filters.push(filter_names[_index].textContent);
-        } else {
-            filters.splice(filters.indexOf(filter_names[_index].textContent), 1);
-        }
-        // делаем новый список продуктов по фильтру
-        renderProducts(filters, products);
+    document.getElementById('sortingSelect').addEventListener('change', () => {
+        renderProducts();
+    });
+    
+    // функция сортировки по выбору ( от меньшего , от большего )
+    function ProductSorted(products = []) {
+        const selectedValue = document.getElementById('sortingSelect').value;
+        products.sort((a, b) => {
+            const priceA = parseInt(a.querySelector('.price span:nth-child(2)').textContent);
+            const priceB = parseInt(b.querySelector('.price span:nth-child(2)').textContent);
+            if (selectedValue === 'asc') {
+                return priceA - priceB;
+            } else {
+                return priceB - priceA;
+            }
+        });
+               
     }
-
-    function renderProducts(filters, products) {
-        const product__container = document.querySelector(".products__container");
-        while (product__container.firstChild) {
-            product__container.removeChild(product__container.firstChild);
-        }
-
+    // функция фильтрации по выбору ( checkboxes )
+    function ProductFilters(products = []) {
+        // получаем массив checkboxes
+        const checkboxes = document.querySelectorAll(".filter__panel__container input");
+        // получаем массив названий checkboxes
+        const filter_names = document.querySelectorAll(".filter__panel__container span");
+        let filters = [];
+        // получаем список выбранных checkbox-ов
+        checkboxes.forEach((chBox, _index) => {
+            if (chBox.checked) {
+                // если стоит галочка добавляем в список фильтрации
+                filters.push(filter_names[_index].textContent);
+            }
+        })
         if (filters.length > 0) {
-            filters.forEach(filter => {
+            let newProducts = [];
+            filters.forEach((filter) => {      
                 products.forEach(product => {
-                    if (filter.toLowerCase() === product.querySelector(".socket").textContent.toLowerCase() || filter.toLowerCase() === product.querySelector(".manufacturer").textContent.toLowerCase()
-                        || filter.toLowerCase() === product.querySelector(".processorLine").textContent.toLowerCase()) {
-                        product__container.appendChild(product);
+                    if (
+                        filter.toLowerCase() === product.querySelector(".socket").textContent.toLowerCase() ||
+                        filter.toLowerCase() === product.querySelector(".manufacturer").textContent.toLowerCase() ||
+                        filter.toLowerCase() === product.querySelector(".processorLine").textContent.toLowerCase()
+                    ) {
+                        newProducts.push(product);
                     }
-                })
-            })
-        } else {
-            products.forEach(product => {
-                product__container.appendChild(product);
-            })
+                });
+            });
+            products.length = 0;
+            products.push(...newProducts);
         }
+    }
+    // функция рендеринга продуктов
+    function renderProducts() {
+
+        // получаем наш новый список продуктов ( с исходными данными )
+        let products = Array.from(SourceProducts);
+        // вызываем функцию сортировки
+        ProductSorted(products);
+        // вызываем функцию фильтрации
+        ProductFilters(products);
+        // очищаем таблицу продуктов
+        const product__table = document.querySelector(".products__container");
+        product__table.querySelectorAll(".show__processor__container").forEach(processor => {
+            processor.remove();
+        })
+        products.forEach(product => {
+            product__table.appendChild(product);
+        })
     }
 });
