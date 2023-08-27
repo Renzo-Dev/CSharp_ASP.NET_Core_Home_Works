510ac6b93c2fd1a8e33d5931f7a11f09f0e353f0
Dan Koshevoy <dankoshevoy2001@gmail.com>
2023-08-27 05:21:51 +0300

    осталось добавить, изменить ( книгу/пользователя) , удалить пользователя

--- Home Work 7/Pages/BookManagement.cshtml.cs
+++ Home Work 7/Pages/BookManagement.cshtml.cs
@@ -1,10 +1,30 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Home_Work_7.Pages;
 
-public class Book_Management : PageModel
+public class BookManagement : PageModel
 {
     public void OnGet()
     {
+
+    }
+
+    public void OnGetDelete(string bookName = "")
+    {
+        Console.WriteLine("WORK");
+        if (bookName.Length > 0)
+        {
+            for (int i = 0; i < Books.books.Count; i++)
+            {
+                Console.WriteLine(i);
+                if (string.Equals(Books.books[i].Title, bookName, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    Books.books.RemoveAt(i);
+                    break;
+                }
+            }
+        }
     }
 }
--- Home Work 7/wwwroot/scripts/BookManagement.js
+++ Home Work 7/wwwroot/scripts/BookManagement.js
@@ -6,12 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
             alert("WORk")
         });
     });
-    // добавляем событие для Button Delete Book
-    const DeleteBookButtons = document.querySelectorAll('#deleteB');
-    DeleteBookButtons.forEach(bDelete => {
-        bDelete.addEventListener('click', () => {
-            alert("WORk")
-        });
+    // добавляем событие для Button Delete Book ( Удаление книги )
+    const table = document.querySelector('.book__table__container table');
+    table.addEventListener('click',(event)=>{
+        const target = event.target;
+        if (target.classList.contains('deleteB')) {
+            const row = target.closest('tr');
+            if (row) {
+                const response = fetch(`/BookManagement?handler=Delete&bookName=${row.querySelector('td:first-child').textContent}`,
+                    {
+                        method: "GET",
+                        headers: {
+                            'Content-Type': 'application/json',
+                            "Accept": "application/json"
+                        }
+                    });
+                window.location.href = "/BookManagement";
+            }
+        }
     });
     // добавляем событие для Button Add New Book
     const AddNewBookButton = document.getElementById('bAddNewBook');
