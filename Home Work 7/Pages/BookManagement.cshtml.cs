bf9932e4edd1b321ab886bffa3bb7d3c5c400db1
Dan Koshevoy <dankoshevoy2001@gmail.com>
2023-08-29 04:49:02 +0300

    update

--- Home Work 7/Pages/BookManagement.cshtml.cs
+++ Home Work 7/Pages/BookManagement.cshtml.cs
@@ -1,79 +1,86 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace Home_Work_7.Pages;
-
-[IgnoreAntiforgeryToken]
-public class BookManagement : PageModel
+namespace Home_Work_7.Pages
 {
-    public void OnGet()
+    [IgnoreAntiforgeryToken]
+    public class BookManagement : PageModel
     {
-    }
-
-    public IActionResult OnGetBook(int id = 0)
-    {
-        var sendBook = Books.books.Find(book => book != null && book.id == id);
-        if (sendBook != null)
+        public IActionResult OnGetBook(int id = 0)
         {
-            var data = new
+            var sendBook = Books.books.Find(book => book != null && book.id == id);
+            if (sendBook != null)
             {
-                Id = sendBook.id, title = sendBook.Title, publicationYear = sendBook.PublicationYear,
-                publisher = sendBook.Publisher, author = sendBook.Author, genre = sendBook.Genre
-            };
-            return new JsonResult(data);
-        }
-
-        return StatusCode(StatusCodes.Status404NotFound);
-    }
-
-    public async Task<IActionResult> OnPostCreate()
-    {
-        var requestBody = await new StreamReader(Request.Body).ReadToEndAsync();
-        var bookData = JsonConvert.DeserializeObject<Book>(requestBody);
-
-        if (bookData != null)
-        {
-            Books.books.Add(bookData);
-            var result = new { Status = "Книга добавлена в библиотеку" }; // Пример результата
-            return new JsonResult(result);
-        }
-        else
-        {
-            HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-            var result = new { Status = "Ошибка , не удалось создать книгу" }; // Пример результата
-            return new JsonResult(result);
-        }
-    }
-
-    public async Task<IActionResult> OnPostUpdate()
-    {
-        var requestBody = await new StreamReader(Request.Body).ReadToEndAsync();
-        var bookData = JsonConvert.DeserializeObject<Book>(requestBody);
-        if (bookData != null)
-        {
-            var index = Books.books.FindIndex(book => book.id == bookData.id);
-            if (index != -1)
-            {
-                Books.books[index] = bookData;
-                return RedirectToPage();
+                var data = new
+                {
+                    Id = sendBook.id,
+                    title = sendBook.Title,
+                    publicationYear = sendBook.PublicationYear,
+                    publisher = sendBook.Publisher,
+                    author = sendBook.Author,
+                    genre = sendBook.Genre
+                };
+                return new JsonResult(data);
             }
 
-            HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-            var result = new { Status = "Ошибка , книга не найдена" }; // Пример результата
-            return new JsonResult(result);
+            return StatusCode(StatusCodes.Status404NotFound);
         }
 
+        public async Task<IActionResult> OnPostCreate()
         {
-            HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-            var result = new { Status = "Ошибка , книга не найдена" }; // Пример результата
-            return new JsonResult(result);
-        }
-    }
+            var requestBody = await new StreamReader(Request.Body).ReadToEndAsync();
+            var bookData = JsonConvert.DeserializeObject<Book>(requestBody);
 
-    public IActionResult OnPostDeleteBook(int id = 0)
-    {
-        Books.books.RemoveAt(Books.books.FindIndex(book => book.id == id));
-        return RedirectToPage();
+            if (bookData != null)
+            {
+                Books.books.Add(bookData);
+                var result = new { Status = "Книга добавлена в библиотеку" };
+                return new JsonResult(result);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                var result = new { Status = "Ошибка, не удалось создать книгу" };
+                return new JsonResult(result);
+            }
+        }
+
+        public async Task<IActionResult> OnPostUpdate()
+        {
+            var requestBody = await new StreamReader(Request.Body).ReadToEndAsync();
+            var bookData = JsonConvert.DeserializeObject<Book>(requestBody);
+
+            if (bookData != null)
+            {
+                var index = Books.books.FindIndex(book => book.id == bookData.id);
+                if (index != -1)
+                {
+                    Books.books[index] = bookData;
+                    return RedirectToPage();
+                }
+                else
+                {
+                    HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                    var result = new { Status = "Ошибка, книга не найдена" };
+                    return new JsonResult(result);
+                }
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                var result = new { Status = "Ошибка, книга не найдена" };
+                return new JsonResult(result);
+            }
+        }
+
+        public IActionResult OnPostDeleteBook(int id = 0)
+        {
+            Books.books.RemoveAt(Books.books.FindIndex(book => book.id == id));
+            return RedirectToPage();
+        }
     }
 }
