1a44085043efa76b3df5b9186d391942f8a847e9
RenzoDev <dankoshevoy2001@gmail.com>
2023-09-04 10:00:01 +0300

    update

---
+++ Home Work 10 MVC/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllerRoute(
+    "default",
+    "{controller=Home}/{action=Index}");
+app.MapControllerRoute(
+    "Processors",
+    "Processors",
+    new { controller = "Processors", action = "ProcessorsResult" });
+app.MapControllerRoute(
+    "About",
+    "About",
+    new { controller = "Home", action = "About" });
+app.MapControllerRoute(
+    "ProcessorDetails",
+    "ProcessorDetails",
+    new { controller = "Processors", action = "ProcessorDetails" });
+app.MapControllerRoute(
+    "MessagesModerator",
+    "MessagesModerator",
+    new { controller = "ModeratorControlls", action = "MessagesModerator" });
+app.MapControllerRoute(
+    "MessagesModerator",
+    "MessagesModerator",
+    new { controller = "ModeratorControlls", action = "MessagesModerator" });
+app.MapControllerRoute(
+    "MessagesModerator",
+    "MessagesModerator",
+    new { controller = "ModeratorControlls", action = "MessagesForModerator" });
+
+app.Run();
